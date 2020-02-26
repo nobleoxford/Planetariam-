d887773e8717b24727c11dbece888da17573b55d
nobleoxford <nobleoxford@amdg.rockhursths.edu>
2020-02-26 08:22:10 -0600

    Hey

---
+++ planets.py
@@ -0,0 +1,18 @@
+class Planet(object):
+
+    def__init__(self, name, mass, temp, color):
+        self.name = name
+        self.mass = mass
+        self.temp = temp
+        self.color = color
+
+    def__str__(self):
+        return f"Name: {self.make}"
+
+    
+
+
+
+
+
+    
